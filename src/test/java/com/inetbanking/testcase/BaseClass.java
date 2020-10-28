0ebb239c3bbe71955744b6c5354f2b432d724e16
shagunvashisht <shagun4android@gmail.com>
2020-10-28 12:17:53 +0530

    Commit done

--- src/test/java/com/inetbanking/testcase/BaseClass.java
+++ src/test/java/com/inetbanking/testcase/BaseClass.java
@@ -1,9 +1,14 @@
 package com.inetbanking.testcase;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
-
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -55,6 +60,13 @@ public class BaseClass {
 	public void tearDown() {
 	driver.quit();
 	//driver.close();
+}
+public void captureScreen(WebDriver driver, String tname) throws IOException {
+	TakesScreenshot ts = (TakesScreenshot) driver;
+	File source = ts.getScreenshotAs(OutputType.FILE);
+	File target = new File(System.getProperty("user.dir") + "/Screenshot/" + tname + ".png");
+	FileUtils.copyFile(source, target);
+	System.out.println("Screenshot taken");
 }
 
 }
